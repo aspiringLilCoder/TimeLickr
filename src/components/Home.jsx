7ba96c99784f42ed150f823aa6c58166f461ef4c
aoi.coder <87404668+aspiringLilCoder@users.noreply.github.com>
2024-04-28 00:26:33 +0900

    🐛Fix:  Invalid DOM property

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,9 +14,9 @@ function Home({ handleSignIn, signedIn }) {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="settingsIcon"
         >
           <circle cx="12" cy="12" r="3"></circle>
--- src/components/TaskManagement.jsx
+++ src/components/TaskManagement.jsx
@@ -24,9 +24,9 @@ function TaskManagement(props) {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <line x1="19" y1="12" x2="5" y2="12"></line>
             <polyline points="12 19 5 12 12 5"></polyline>
@@ -109,10 +109,11 @@ function TaskManagement(props) {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       className="website-add-btn"
+                      onClick={addWebsite}
                     >
                       <rect
                         x="3"
