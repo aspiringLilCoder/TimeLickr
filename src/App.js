4ac7bdb83a837fdd93143bad9a868415cd5de79e
aoi.coder <87404668+aspiringLilCoder@users.noreply.github.com>
2024-04-27 18:57:25 +0900

    ✨Feat: create main components and router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Home from "./components/Home";
+import TaskManagement from "./components/TaskManagement";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/tasks" element={<TaskManagement />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
