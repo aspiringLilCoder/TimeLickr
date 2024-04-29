b015ec46055cdb17b322cc5ebec06f6862b82b9f
aoi.coder <87404668+aspiringLilCoder@users.noreply.github.com>
2024-04-30 02:21:02 +0900

    ✨Feat: styling for warning page

--- public/Content.js
+++ public/Content.js
@@ -6,26 +6,37 @@ chrome.storage.local.get(["key"]).then((result) => {
   taskArray = result.key;
 
   taskArray.forEach((task) => {
+    console.log(task);
     if (
       new Date().getTime() >= new Date(task.startTime).getTime() &&
       new Date().getTime() <= new Date(task.endTime).getTime()
     ) {
       const allowOrBlock = task.allowOrBlock;
+      const html =
+        '<div style="color:#333; display: flex; flex-direction: column; align-items: center; justify-content: center; position:absolute; width:100%; height:100%; z-index:100; background-color:#BAF0FA; top:0; left:0;">' +
+        `<p style="font-weight: bold; margin-top: 10px; font-size: 30px">Time is lickin\' by! Get back to work until ${new Date(
+          task.endTime
+        ).toLocaleTimeString([], {
+          hour: "numeric",
+          minute: "2-digit",
+        })} ;)</p>` +
+        "</div>";
 
       task.websiteList.forEach((website) => {
         if (allowOrBlock == "allow") {
           if (website === fullURL || website === shortURL) {
             console.log("in whitelist");
+            return;
           } else {
             console.log("not in whitelist");
-            document.body.innerHTML =
-              '<div style="color:white; display: flex; align-items: center; justify-content: center; position:absolute;width:100%;height:100%;z-index:100;background-color:black;top:0;left:0;">Get back to work :)</div>';
+            document.body.innerHTML = html;
+            return;
           }
         } else {
           if (website === fullURL || website === shortURL) {
             console.log("in block list");
-            document.body.innerHTML =
-              '<div style="color:white; display: flex; align-items: center; justify-content: center; position:absolute;width:100%;height:100%;z-index:100;background-color:black;top:0;left:0;">Get back to work :)</div>';
+            document.body.innerHTML = html;
+            return;
           }
         }
       });
